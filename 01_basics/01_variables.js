127eafa1adf651d1da133fd610a9600e83ef7f01
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-05-29 05:42:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345"
+accountCity= "Jaipur"
+
+
+// accountId = 2
+
+
+accountEmail = "hc@hc.com"
+accountPassword="123123"
+accountCity="rajasthan"
+let accountState;
+
+console.log(accountId)
+
+
+/*
+Prefred not to use var
+because of issue in block space and functional scope
+
+*/
+console.table([accountId,accountEmail,accountCity,accountState])
+
+
+
+
+
+
+
+console.log(accountId);
+
---
+++ 01_basics/02_variables.js
@@ -0,0 +1,9 @@
+
+"use strict"; //treat all js code as newer version
+
+// alert( 3 + 3)// used in browser not in node js
+
+// console.log(3+3);
+console.log("Hitesh");
+
+console.log(typeof null)
