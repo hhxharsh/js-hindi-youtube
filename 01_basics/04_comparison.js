7a7018e3d35b709accf1f1375d9192f9af460afb
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-05-29 11:27:44 +0000

    datatypessummary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+
+//in jasvascript null is covert to a number therfore>= true and> givwes false
+
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+console.log(undefined > 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+
+// ===
+
+console.log(2==="2")
+
+console.log(2 =="2")
---
+++ datatypes-summary.js
@@ -0,0 +1,33 @@
+
+const score= 100
+
+const scoreval=100.3
+
+const isLoggedIn = false;
+
+const outsideTemp = null;
+
+let userEmail;
+
+
+//def of symbol
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 2346n
+
+const heros = ["shaktiman","naagraj","doga"]
+
+let myObj  = { name:"hitesh",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof heros)
