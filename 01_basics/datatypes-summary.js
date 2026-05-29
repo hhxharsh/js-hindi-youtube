0e6a7760988a9e8b856924b0fff9942dcad5f89e
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-05-29 12:54:57 +0000

    stack and heaps in javascript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,7 +1,6 @@
+const score = 100;
 
-const score= 100
-
-const scoreval=100.3
+const scoreval = 100.3;
 
 const isLoggedIn = false;
 
@@ -9,25 +8,42 @@ const outsideTemp = null;
 
 let userEmail;
 
-
 //def of symbol
 
-const id= Symbol('123')
-const anotherId= Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 console.log(id === anotherId);
 
+const bigNumber = 2346n;
 
-const bigNumber = 2346n
+const heros = ["shaktiman", "naagraj", "doga"];
 
-const heros = ["shaktiman","naagraj","doga"]
+let myObj = { name: "hitesh", age: 22 };
 
-let myObj  = { name:"hitesh",
-    age:22,
-}
+const myFunction = function () {
+  console.log("Hello world");
+};
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+console.log(typeof heros);
 
-console.log(typeof heros)
+let myYoutubename = "hiteshchoudharydotcom";
+
+let anothername = myYoutubename;
+anothername = "chaiaurcode";
+
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+console.log(userOne.email);
+userTwo.email = "hitesh@google.com";
+console.log(userTwo.email);
+
+console.log(userOne.email);
