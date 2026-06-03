53242e6808e98f7e7013653a818d4ccca094f953
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-06-03 15:34:19 +0000

    destructuringinjs

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,35 @@
+const course = {
+    coursename:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+course.courseInstructor
+const {courseInstructor:instructor} =course
+console.log(instructor);
+
+//destructuting the objects
+// const navbar = ()=> {
+
+// }
+
+// console.log(navbar(company = "hitesh"))
+
+
+//apna kam kisi ke aur k sar par dall dena
+//waiter ko order dena
+
+
+// {
+//     "coursename":"js in hindi",
+//     "price":"free",
+//     "name":"harsh"
+// }
+
+//api in form of array
+
+[
+    {},
+    {},
+    {},
+]
