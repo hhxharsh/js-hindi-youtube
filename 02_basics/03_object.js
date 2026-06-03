9c59386c17ed8b82d282a8ea01cef4c61833a176
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-06-03 13:35:46 +0000

    objectsinjs

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -7,7 +7,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name:"Harsh",
     "full name":"Harsh Singh",
-    mySym="mykey1",
+    [mySym]:"mykey1",
     age:18,
     location:"jaipur",
     email:"singharsh5@gmail.com",
@@ -15,9 +15,29 @@ const JsUser = {
     lastLoginDays:["Monday","Tuesday"]
 }
 
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["full name"])
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+//= overwrite value 
+JsUser.email = "singh@gmail.com";
+
+//how to freesze or lock values
+
+// Object.freeze(JsUser)
+
+JsUser.email = "singh@amazon.com"
 
 
+console.log(JsUser)
 
+JsUser.greeting = function(){
+    console.log("HelloJsUser")
+}
+JsUser.greetingtwo= function(){
+    console.log(`HelloJsUser,${this.name}`)
+}
+console.log(JsUser.greeting())
+console.log(JsUser.greetingtwo());
