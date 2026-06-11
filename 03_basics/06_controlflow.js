ea09dbb9c385d9bd53b962782d1a3499cdd77f2c
hhxharsh <168496892+hhxharsh@users.noreply.github.com>
2026-06-11 11:41:46 +0000

    objectsaccessinginarray

---
+++ 03_basics/06_controlflow.js
@@ -0,0 +1,20 @@
+accessing objectinsidearrray
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "rust",
+        languageFileName: "r"
+    },
+    {
+        languageName: "pandas",
+        languageFileName: "python"
+    },
+]
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+   // console.log(item.languageFileName);
+})
